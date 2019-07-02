c27fe571a26fe9016028e6c9b9c10820156b2db9
guwenpeng <wenpeng.gu@huilianyi.com>
2019-07-02 15:28:06 +0800

    first commit GWP

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好,世界 Hello World !!!");
+
+            Console.ReadKey();
+        }
+    }
+}
